6d662e4a25b02a2fd24282e3873870c7a29b8730
Michael Chavez III <mchavez@pbs.org>
2014-05-29 10:56:08 -0400

    commit from visual studio

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderPath = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.destinationPath = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source Folder for unformatted XML";
+            // 
+            // folderPath
+            // 
+            this.folderPath.Location = new System.Drawing.Point(264, 16);
+            this.folderPath.Name = "folderPath";
+            this.folderPath.Size = new System.Drawing.Size(75, 23);
+            this.folderPath.TabIndex = 1;
+            this.folderPath.Text = "Path";
+            this.folderPath.UseVisualStyleBackColor = true;
+            this.folderPath.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Destination Folder for \'pretty\' XML";
+            // 
+            // destinationPath
+            // 
+            this.destinationPath.Location = new System.Drawing.Point(264, 19);
+            this.destinationPath.Name = "destinationPath";
+            this.destinationPath.Size = new System.Drawing.Size(75, 23);
+            this.destinationPath.TabIndex = 4;
+            this.destinationPath.Text = "Destination";
+            this.destinationPath.UseVisualStyleBackColor = true;
+            this.destinationPath.Click += new System.EventHandler(this.destinationPath_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 226);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(210, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Override files in Source folder location?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Pretty the XML!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(387, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 43);
+            this.listBox1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.folderPath);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 100);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Source";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.destinationPath);
+            this.groupBox2.Location = new System.Drawing.Point(12, 113);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(345, 96);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Destination";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 273);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Pretty up your XML files";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button folderPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button destinationPath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo dinfo = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                FileInfo[] Files = dinfo.GetFiles("*.xml");
+                foreach (FileInfo file in Files)
+                {
+                    listBox1.Items.Add(file.Name);
+                }
+
+                this.label2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            foreach (var file in listBox1.Items)
+            {
+                String xmlFileToBeFormatted = Path.Combine(this.label2.Text, file.ToString());
+                String locationOfFormattedXml = "";
+                if (this.checkBox1.Checked)
+                {
+                    locationOfFormattedXml = xmlFileToBeFormatted;
+                }
+                string xDoc = LoadXml(xmlFileToBeFormatted);
+                XDocument formattedDoc = FormatXml(xDoc);
+
+                if (this.label4.Text.Length > 0)
+                {
+                    locationOfFormattedXml = Path.Combine(this.label4.Text, file.ToString());
+                }
+                WriteXml(formattedDoc, locationOfFormattedXml);
+                //var boo = formattedDoc;
+            }
+        }
+
+        public string LoadXml(String filename)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(filename);
+                return doc.ToString();
+            }
+            catch (Exception)
+            {
+                return filename;
+            }
+
+        }
+
+        public XDocument FormatXml(String Xml)
+        {
+            try
+            {
+                XDocument doc = XDocument.Parse(Xml);
+                return doc;
+            }
+            catch (Exception)
+            {
+                return new XDocument();
+            }
+        }
+        public void WriteXml(XDocument formattedXml, String locationOfXml)
+        {
+            try
+            {
+                formattedXml.Save(locationOfXml);
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void destinationPath_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.label4.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckState state = checkBox1.CheckState;
+
+            if (state == CheckState.Checked)
+            {
+                this.destinationPath.Enabled = false;
+            }
+            else
+            {
+                this.destinationPath.Enabled = true;
+            }
+        }
+    }
+}
